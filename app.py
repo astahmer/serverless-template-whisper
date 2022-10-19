15487a1442990cb7cc5a569a8426bc0d27874eb4
Alexandre Stahmer <47224540+astahmer@users.noreply.github.com>
2022-10-19 17:04:37 +0200

    feat: allow passing lang/task & set model to small

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from io import BytesIO
 def init():
     global model
     
-    model = whisper.load_model("base")
+    model = whisper.load_model("small")
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -18,6 +18,9 @@ def inference(model_inputs:dict) -> dict:
 
     # Parse out your arguments
     mp3BytesString = model_inputs.get('mp3BytesString', None)
+    lang = model_inputs.get('lang', None)
+    task = model_inputs.get('task', 'transcribe')
+
     if mp3BytesString == None:
         return {'message': "No input provided"}
     
@@ -26,7 +29,7 @@ def inference(model_inputs:dict) -> dict:
         file.write(mp3Bytes.getbuffer())
     
     # Run the model
-    result = model.transcribe("input.mp3")
+    result = model.transcribe("input.mp3", language=lang, task=task, beam_size=5, best_of=5)
     output = {"text":result["text"]}
     os.remove("input.mp3")
     # Return the results as a dictionary
